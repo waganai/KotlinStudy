ddd65cbe143eeca2dcd2ddcfba25806e85c9fc5f
waganai <358973601@qq.com>
2021-04-26 00:06:17 +0800

    [Java][泛型]泛型

--- src/main/java/generic/GenericTestMain.java
+++ src/main/java/generic/GenericTestMain.java
@@ -10,15 +10,26 @@ public class GenericTestMain<T extends Animal> {
         arrayList.add(Integer.valueOf(2));
         arrayList.add(Integer.valueOf(3));
         arrayList.add(Integer.valueOf(4));
+
         List<? extends Number> list1 = new ArrayList<Integer>(arrayList);
 
         for (int i = 0; i < list1.size(); i++) {
             System.out.println("list1.get(" + i + ") = " + list1.get(i));
         }
 
+//        Object object = list1.get(0);
+        Number integer  = list1.get(0);
+
         List<? super Cat> list2 = new ArrayList<Animal>();
         list2.add(new Cat());
         list2.add(new WhiteTiger());
+
+        System.out.println("list2.get(0) instanceof Object == " + list2.get(0));
+        System.out.println("list2.get(1) instanceof Object == " + list2.get(1));
+
+        // 对于 <? super T>，只能往内部设置值，不能往外读取值，除非是Object object =
+        Object object = list2.get(0);
+        list2.set(0, new Tiger());
 
         System.out.println("list2.get(0) instanceof Object == " + list2.get(0));
 
