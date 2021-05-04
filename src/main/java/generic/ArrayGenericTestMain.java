b1ab54d9fa47ff84e1392c476ad8f547090fea85
waganai <358973601@qq.com>
2021-05-04 21:29:11 +0800

    [泛型][数组]Java泛型

---
+++ src/main/java/generic/ArrayGenericTestMain.java
@@ -0,0 +1,19 @@
+package generic;
+
+import java.util.ArrayList;
+
+public class ArrayGenericTestMain {
+    public static void main(String[] args) {
+        Object[] objects;
+        String[] strs = new String[10];
+        objects = strs;
+//        objects[0] = Integer.valueOf(0);
+        ArrayList<String> arrayList = new ArrayList<String>();
+        arrayList.add("666");
+
+        System.out.println("strs' type is" + strs.getClass());
+        System.out.println("arrayList' type is" + arrayList.getClass());
+
+//        ArrayList<Object> objectList = arrayList;
+    }
+}
