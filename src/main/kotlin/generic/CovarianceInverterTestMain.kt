6093bb5a627f3de6f616512b112336ea1f0ae9d3
waganai <358973601@qq.com>
2021-05-05 15:28:35 +0800

    [Kotlin][泛型]Kotlin获取泛型的类型

---
+++ src/main/kotlin/generic/CovarianceInverterTestMain.kt
@@ -0,0 +1,9 @@
+package generic
+
+class CovarianceInverterTestMain {
+}
+
+fun main() {
+    val stringList: List<String> = ArrayList<String>()
+    val anyList: List<Any> = stringList
+}
--- src/main/kotlin/generic/KotlinGenericTestMain.kt
+++ src/main/kotlin/generic/KotlinGenericTestMain.kt
@@ -6,12 +6,16 @@ class KotlinGenericTestMain {
         fun <T> getClassType(clazz: Class<T>) {
             println("clazz 'type is $clazz")
         }
+
+        inline fun <reified T : Any> getClazzType(t: T) {
+            println("type is ${T::class.java}")
+        }
     }
 }
 
 fun main() {
-    val arrayList = ArrayList<String>()
-    val arrayList2 = object:ArrayList<String>(){}
+    val arrayList = arrayListOf("1", "2", "3")
+    val arrayList2 = object : ArrayList<String>() {}
     val array = arrayOfNulls<String>(10)
 
     println("arrayList' type is ${arrayList.javaClass}")
@@ -29,9 +33,21 @@ fun main() {
     KotlinGenericTestMain.getClassType(array2::class.java)
 
     println()
+    println("********************************************")
 
     println("arrayList' type is ${arrayList.javaClass.genericSuperclass}")
     println("arrayLis2' type is ${arrayList2.javaClass.genericSuperclass}")
+
+    println()
+    println("********************************************")
+
+    KotlinGenericTestMain.getClazzType(Parent())
+    KotlinGenericTestMain.getClazzType(Child())
+    KotlinGenericTestMain.getClazzType(arrayList)
+    KotlinGenericTestMain.getClazzType(arrayList2)
+    KotlinGenericTestMain.getClazzType(array)
+    KotlinGenericTestMain.getClazzType(array1)
+    KotlinGenericTestMain.getClazzType(array2)
 }
 
 class Parent {
